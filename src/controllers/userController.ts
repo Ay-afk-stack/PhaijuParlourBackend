6800f8558c141a2704a8635e2103713ca3615e52
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2024-11-04 23:17:44 +0545

    Basic Login finish

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -26,6 +26,37 @@ class UserController {
       console.error("Error:", error);
     }
   }
+  static async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        success: false,
+        message: "Please enter credentials!",
+      });
+      return;
+    }
+
+    try {
+      const [user] = await User.findAll({ where: { email: email } });
+
+      if (!user) {
+        res
+          .status(404)
+          .json({ success: false, messsage: "No user found with that email!" });
+      } else {
+        const isEqual = bcrypt.compareSync(password, user.password);
+        if (!isEqual) {
+          res
+            .status(400)
+            .json({ success: false, message: "Invalid Credentials" });
+        } else {
+          res.status(200).json({ success: true, message: "Login Successful!" });
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 export default UserController;
